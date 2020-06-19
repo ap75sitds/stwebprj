ae7a96f93cabe007d3b506f12e9890c31d837c3d
ap75sitds <alexey.petuhoff@gmail.com>
2020-06-19 10:12:20 +0300

    lesson2-02

--- vgprox/wsgi.py
+++ vgprox/wsgi.py
@@ -2,7 +2,7 @@ def app(environ, start_response):
     '''po zaprosu http://127.0.0.1:8000/?a=1&a=3&b=0
     app vozvrashaet v stolbik znacheniya'''
     qs = environ['QUERY_STRING']
-    s = qs.replace('&','\n')
+    s = qs.replace('&','<br>')
     data = s.encode()
     print(data)
     start_response('200 OK', [('Content-type', 'text/plain')])
