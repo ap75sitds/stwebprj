582c0671d86cef8550e8dbdd2088b622551cf589
ap75sitds <alexey.petuhoff@gmail.com>
2020-07-06 01:12:40 +0300

    sql-lesson4

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -10,7 +10,7 @@ class QuestionManager(models.Manager):
         return self.order_by('-rating')
 
 class Question(models.Model):
-    object = QuestionManager()
+    objects = QuestionManager()
     title = models.CharField(max_length=200)
     text = models.TextField()
     added_at = models.DateTimeField(auto_now_add=True)
