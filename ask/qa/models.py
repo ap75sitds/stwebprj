035df929870bb7e844047cd78efe63f28007f220
ap75sitds <alexey.petuhoff@gmail.com>
2020-07-09 01:51:14 +0300

    Views, models, pagination

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.http import Http404
 from django.utils import timezone
 from django.contrib.auth.models import User
 import datetime
@@ -8,6 +9,17 @@ class QuestionManager(models.Manager):
         return self.order_by('-added_at')
     def popular(self):
         return self.order_by('-rating')
+    def question_on_page(self, qid):
+        try :
+            z = self.get(id=qid)
+            ans = Answer.objects.filter(question_id=qid)
+        except :
+            raise Http404
+        answer_to_quest = []
+        for a in ans :
+            answer_to_quest.append(a.text)
+        fdic = {'title':z.title, 'text':z.text, 'answers':answer_to_quest}
+        return fdic
 
 class Question(models.Model):
     objects = QuestionManager()
@@ -17,9 +29,14 @@ class Question(models.Model):
     rating = models.IntegerField(default=0)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     likes = models.ManyToManyField(User, related_name='question_like_user')
+ 
+    def __str__(self):
+        return self.title
 
 class Answer(models.Model):
     text = models.TextField()
     added_at = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.text
--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,4 +1,13 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
+from .models import Question, Answer
+
 def test(request, *args, **kwargs):
+    
     return HttpResponse('Firstclass App')
-# Create your views here.
+
+def one_question(request, *args, **kwargs):
+    qid = kwargs['qid']
+    resp = Question.objects.question_on_page(qid)
+    return render(request, 'qa/index.html', resp)
