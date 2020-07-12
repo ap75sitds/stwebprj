8c0ed7397c7271f6f9febd8db89a6076b3b88e67
ap75sitds <alexey.petuhoff@gmail.com>
2020-07-12 17:40:53 +0300

    author null=true

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -27,7 +27,7 @@ class Question(models.Model):
     text = models.TextField()
     added_at = models.DateTimeField(auto_now_add=True)
     rating = models.IntegerField(default=0)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     likes = models.ManyToManyField(User, related_name='question_like_user')
 
     def __str__(self):
