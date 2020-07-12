59e11f525ddb4c6d64d6f5fb0c4a41201723cb45
ap75sitds <alexey.petuhoff@gmail.com>
2020-07-12 17:12:39 +0300

    bash file

--- init_upd.sh
+++ init_upd.sh
@@ -7,7 +7,7 @@ sudo ln -s /usr/bin/python3.5 /usr/bin/python3
 sudo pip3 install --upgrade pip
 sudo pip3 install --upgrade django==2.1
 sudo pip3 install --upgrade gunicorn
-sudo mv nginx.conf nginx_alex.conf
-sudo mv nginx_b.conf nginx.conf
+sudo mv etc/nginx.conf etc/nginx_alex.conf
+sudo mv etc/nginx_b.conf etc/nginx.conf
 
 
